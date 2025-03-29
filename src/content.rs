a39eefe56ccce90b2415c6af536a3306f8389340
nygrenadam <74379763+nygrenadam@users.noreply.github.com>
2025-03-29 22:33:19 +0100

    genesis 2.0

---
+++ src/content.rs
@@ -0,0 +1,7 @@
+// src/content.rs
+
+pub const PARAGRAPH_1: &str = "Exploring the intersection of sound and space. \
+    Creating immersive auditory experiences that challenge \
+    conventional perceptions of electronic music.";
+
+pub const PARAGRAPH_2: &str = "[New album coming 2025] [Selected works available on request]";
