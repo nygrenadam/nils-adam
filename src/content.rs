4d61b34f7fa82840019f4217581d05b7e02f1e84
nygrenadam <adamnygren10@gmail.com>
2025-03-29 23:09:13 +0100

    removed linker

--- src/content.rs
+++ src/content.rs
@@ -1,7 +1,5 @@
 // src/content.rs
 
-pub const PARAGRAPH_1: &str = "Exploring the intersection of sound and space. \
-    Creating immersive auditory experiences that challenge \
-    conventional perceptions of electronic music.";
+pub const PARAGRAPH_1: &str = " ";
 
-pub const PARAGRAPH_2: &str = "[New album coming 2025] [Selected works available on request]";
+pub const PARAGRAPH_2: &str = "new album soon";
